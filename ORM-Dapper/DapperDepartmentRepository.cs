0530724079ec69661156c0062d12a9e584b7f35b
Geoffrey Wells <Gwells625@gmail.com>
2025-08-14 12:11:08 -0400

    setup dapperfunctionality

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Data;
+using Dapper;
+
+namespace ORM_Dapper;
+
+public class DapperDepartmentRepository : IDepartmentRepository
+{
+    private readonly IDbConnection _connection;
+    
+    public DapperDepartmentRepository(IDbConnection connection)
+    {
+        _connection = connection;
+    }
+
+   public IEnumerable<Department> GetAllDepartments()
+    {
+        return _connection.Query<Department>("SELECT * FROM Department");
+    }
+}
